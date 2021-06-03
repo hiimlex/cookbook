70728c817ae584c4980fdd9c44b70f07ab8cfb10
Alex <alekiis.kun@gmail.com>
2021-06-03 19:28:42 -0300

    asdas

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,6 @@
 module.exports = {
 	type: "postgres",
 	url: process.env.DATABASE_URL,
-	extra: {
-		ssl: process.env.SSL,
-	},
 	entities: ["dist/app/models/*.js"],
 	migrations: ["dist/database/migrations/*.js"],
 	cli: { migrationsDir: "src/database/migrations" },
