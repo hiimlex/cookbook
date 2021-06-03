c8e4df731ba9e14147a79a4851199996605d26b9
Alex <alekiis.kun@gmail.com>
2021-06-03 17:27:45 -0300

    build

---
+++ ormconfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+	type: "postgres",
+	url: process.env.DATABASE_URL,
+	entities: ["dist/app/models/*.js"],
+	migrations: ["dist/database/migrations/*.js"],
+	cli: { migrationsDir: "src/database/migrations" },
+};
